5c2cbf29c0c0445f3bbfed6e1c0ee7bbc3c64f95
hzc <hzc@localhost>
2020-02-08 16:35:30 +0800

    增加remember-me的超时时间配置

--- src/main/java/com/maple/security/core/properties/BrowserProperties.java
+++ src/main/java/com/maple/security/core/properties/BrowserProperties.java
@@ -12,6 +12,11 @@ public class BrowserProperties {
 	private String loginPage = SecurityConstants.DEFAULT_SIGN_IN_PAGE_URL;
 	
 	private LoginType loginType = LoginType.REDIRECT;
+	
+	/**
+	 * 记住我的超时时间
+	 */
+	private int rememberMeSeconds = 3600;
 
 	public String getLoginPage() {
 		return loginPage;
@@ -27,6 +32,14 @@ public class BrowserProperties {
 
 	public void setLoginType(LoginType loginType) {
 		this.loginType = loginType;
+	}
+
+	public int getRememberMeSeconds() {
+		return rememberMeSeconds;
+	}
+
+	public void setRememberMeSeconds(int rememberMeSeconds) {
+		this.rememberMeSeconds = rememberMeSeconds;
 	}
 
 }
--- src/main/java/com/maple/security/core/validate/code/image/ImageValidateCodeGenerator.java
+++ src/main/java/com/maple/security/core/validate/code/image/ImageValidateCodeGenerator.java
@@ -9,6 +9,7 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -27,6 +28,7 @@ import com.maple.security.core.validate.code.ValidateCodeGenerator;
 @Component("imageValidateCodeGenerator")
 public class ImageValidateCodeGenerator implements ValidateCodeGenerator {
 
+	@Autowired
 	private SecurityProperties securityProperties;
 
 	@Override
